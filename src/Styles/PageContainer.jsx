b55957622dd120297b405ef8d7739f021dc6be5d
JangGeonil <wkdrjsdlf2@gmail.com>
2020-08-06 01:47:49 +0900

    프로젝트 진행에 필요한 기본 스타일 적용코드 작성

---
+++ src/Styles/PageContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../index.css';
+import styled from 'styled-components';
+
+const Container = styled.section`
+  width: 381px;
+  height: 678px;
+  background-color: var(--color-white);
+  margin: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow: hidden;
+`;
+
+export default function PageContainer({ children }) {
+  return <Container>{children}</Container>;
+}
