52d9c719adc7943b5dee103ffb18dccfa5507dff
JangGeonil <wkdrjsdlf2@gmail.com>
2020-08-06 01:35:07 +0900

    프로젝트 utils 추가

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,3 +1,21 @@
 export function get(key) {
   return (state) => state[key];
 }
+export function get(key) {
+  return (state) => state.chat[key];
+}
+
+export function getYMD(date) {
+  return date.slice(0, 10);
+}
+
+export function makeNewYMN() {
+  const date = new Date();
+  const ymd =
+    ('00' + (date.getMonth() + 1)).slice(-2) +
+    '/' +
+    ('00' + date.getDate()).slice(-2) +
+    '/' +
+    date.getFullYear();
+  return ymd;
+}
