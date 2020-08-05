ff4ea9af33aa02646bb41ebfd4b3cb4f30455196
JangGeonil <wkdrjsdlf2@gmail.com>
2020-08-06 01:50:47 +0900

    [스타일] 배지 테스트 코드 작성

---
+++ src/Styles/Badge.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Badge from './Badge';
+
+test('Badge', () => {
+  const { container } = render(<Badge>jgi</Badge>);
+  expect(container).toHaveTextContent('jgi');
+});
